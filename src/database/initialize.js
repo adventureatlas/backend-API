f8f648e760693f3f2aaa1c2bdf214f8184da388d
Talut Salako <salakotalut@gmail.com>
2024-05-09 20:14:29 +0100

    fix sequelize config opts

--- src/database/initialize.js
+++ src/database/initialize.js
@@ -1,13 +1,14 @@
 import { Sequelize } from "sequelize";
-// import dotenv from "dotenv";
+import dotenv from "dotenv";
 import mysql from "mysql2";
 
-// dotenv.config();
+dotenv.config();
 
 export async function connectToDatabase() {
   const password = process.env.DBPASSWORD;
   const user = process.env.DBUSER;
-  const sequelize = new Sequelize("database", user, password, {
+  const database = process.env.DATABASE;
+  const sequelize = new Sequelize(database, user, password, {
     dialect: "mysql",
     host: process.env.HOST,
     port: process.env.DBPORT,
