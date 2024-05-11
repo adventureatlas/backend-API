6e48414899c53273a8451cdb147b9005482c19da
Talut Salako <salakotalut@gmail.com>
2024-05-11 07:54:27 +0100

    remove password check

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -4,15 +4,13 @@ const register = async (req, res) => {
   try {
     const { firstName, lastName, username, email, password, confirmPassword } =
       req.body; // what other infomation would be neccesary for collection in register process
-    if (password !== confirmPassword) {
-      const newUser = await authService.register(
-        firstName,
-        lastName,
-        username,
-        email,
-        password,
-      );
-    }
+    const newUser = await authService.register(
+      firstName,
+      lastName,
+      username,
+      email,
+      password,
+    );
   } catch (err) {
     console.log(err);
     res.status(err.statusCode || 500).json({ message: err.message });
