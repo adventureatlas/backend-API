96ecd58605af28c8c26d40125673ea4919e0268b
Talut Salako <salakotalut@gmail.com>
2024-05-12 09:38:21 +0100

    better validation checks for password fields

--- src/validation/auth.validation.js
+++ src/validation/auth.validation.js
@@ -5,6 +5,15 @@ export const registerSchema = Joi.object({
   lastName: Joi.string().not("").required(),
   email: Joi.string().email().required(),
   dateOfBirth: Joi.date().iso().required(),
-  password: Joi.string().min(8).required(), // add more validation?
+  password: Joi.string()
+    .min(8)
+    .regex(/^(?=.*[A-Z])(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}$/)
+    .required()
+    .messages({
+        'string.base': 'Password must be a string',
+        'string.empty': 'Password is required',
+        'string.min': 'Password must be at least 8 characters long',
+        'string.pattern.base': 'Password must contain at least one uppercase letter and one symbol',
+    })
   confirmPassword: Joi.valid(Joi.ref("password")).required(),
 });
